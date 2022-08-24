9e3edb5b42cdf03df52879776e194026f5fc60ee
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-24 14:53:22 +0300

    Четное/нечетное

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if ((number % 2) ==0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
