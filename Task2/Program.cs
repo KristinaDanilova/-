c7187cf9596265c8db822ed35e0a7148d20bb515
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-24 14:40:06 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+    Console.WriteLine($"min = {b}");
+} 
+else if (a < b)
+{
+    Console.WriteLine($"min = {a}");
+    Console.WriteLine($"max = {b}");
+}
+else {
+    Console.WriteLine("Введённые числа равны");
+}
