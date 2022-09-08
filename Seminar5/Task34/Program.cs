4e4b73e5f81bfeb02aa1d3bd0855f0bd6d6d6c9f
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-08 11:03:46 +0300

    Update Program.cs

--- Seminar5/Task34/Program.cs
+++ Seminar5/Task34/Program.cs
@@ -2,9 +2,9 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int [] GetArray(int size, int minValue, int maxValue)
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    int [] res = new int[size];
+    int[] res = new int[size];
     for (int i = 0; i < size; i++)
     {
         res[i] = new Random().Next(minValue, maxValue + 1);
