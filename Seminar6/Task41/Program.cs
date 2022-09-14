5491773dd5fac3d906f269f314b5cf43b8f43923
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-14 11:55:20 +0300

    сколько чисел больще 0 ввел польз-ль

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+
+int Num (int[] arr)
+{
+int count = 0; 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+return count;
+}
+
+Console.Write("Сколько чисел необходимо ввести: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine($"Количество чисел > 0: {Num(arr)}");
