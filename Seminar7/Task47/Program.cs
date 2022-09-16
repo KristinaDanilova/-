1a1ea00a8ea6d5d7f12c60f5164a99911d7c580d
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-16 13:01:38 +0300

    двумерный массив m*n, заполнен вещественными числами

---
+++ Seminar7/Task47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(-10, 10) + new Random().NextDouble();
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] myArray = GetArray(m, n);
+PrintArray(myArray);
